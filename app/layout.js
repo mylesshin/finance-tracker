c1fe3f4db2f6a50a62b3d371d2bac53c172e6bbd
mylesshin <myles.h.shin@vanderbilt.ediu>
2023-04-09 14:26:53 -0500

    updated to firebase

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,25 @@
-import './globals.css'
+"use client";
+
+import "./globals.css";
 
 import Nav from "@/components/Navigation";
+
+import FinanceContextProvider from "@/lib/store/finance-context";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
       <body>
-        <Nav />{children}</body>
+        <FinanceContextProvider>
+          <Nav />
+          {children}
+        </FinanceContextProvider>
+      </body>
     </html>
-  )
+  );
 }
